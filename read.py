4ecf8454c8828b509d8f7d534c2821c4767e9e27
ramla <grammler@gmail.com>
2026-03-01 20:09:01 +0200

    batch convert everything from ./dump to csv if no file given

--- read.py
+++ read.py
@@ -3,62 +3,67 @@ import csv
 import os
 import sys
 
-# if len(sys.argv) < 2:
-#     print("Usage: python read.py dump_filename[s]", file=sys.stderr)
-#     exit(1)
-# filename = sys.argv[1]
+filenames = []
+if len(sys.argv) < 2:
+    f = os.listdir("./dump")
+    filenames = ["./dump/" + filename for filename in os.listdir("./dump") if filename[-5:] == ".dump"]
+else:
+    filenames = sys.argv[1:]
 
-filename = "dump/Rip.dump"
+for filename in filenames:
+    dump = None
+    with open(filename, "r", encoding="utf-8") as file:
+        dump = file.readline()
 
-dump = None
-with open(filename, "r", encoding="utf-8") as file:
-    dump = file.readline()
+    dump = ast.literal_eval(dump)    
 
-dump = ast.literal_eval(dump)    
+    playlist_name = "saved" # default: liked songs, list of items
+    itemcontainer = dump
+    data = []
+    keys = []
 
-playlist_name = "saved" # default: liked songs, list of items
-itemcontainer = dump
-data = []
-keys = []
+    if type(dump) == dict:
+        # playlist dump
+        # playlist_keys: ['collaborative', 'description', 'external_urls', 'followers', 'href', 'id', 'images',
+        #                 'name', 'owner', 'primary_color', 'public', 'snapshot_id', 'tracks', 'items', 'type', 'uri']
+        playlist_name = dump["name"]
+        itemcontainer = dump["items"]
 
-if type(dump) == dict:
-    # playlist dump
-    # playlist_keys: ['collaborative', 'description', 'external_urls', 'followers', 'href', 'id', 'images',
-    #                 'name', 'owner', 'primary_color', 'public', 'snapshot_id', 'tracks', 'items', 'type', 'uri']
-    playlist_name = dump["name"]
-    itemcontainer = dump["items"]
-
-for item in itemcontainer:
-    # item_keys = ['added_at', 'added_by', 'is_local', 'primary_color', 'track', 'item', 'video_thumbnail']
-    # track_keys = ['preview_url', 'available_markets', 'explicit', 'type', 'episode', 'track', 'album', 'artists',
-    #               'disc_number', 'track_number', 'duration_ms', 'external_ids', 'external_urls', 'href', 'id',
-    #               'name', 'popularity', 'uri', 'is_local']
-    # artist_keys = ['external_urls', 'href', 'id', 'name', 'type', 'uri']
-    # album_keys = ['available_markets', 'type', 'album_type', 'href', 'id', 'images', 'name', 'release_date',
-    #               'release_date_precision', 'uri', 'artists', 'external_urls', 'total_tracks']
-    trackname = item["track"]["name"]
-    artistnames = [artist["name"] for artist in item["track"]["artists"]]
-    albumname = item["track"]["album"]["name"]
-    year = item["track"]["album"]["release_date"].split("-")[0]
-    print(f"{artistnames[0]} - {trackname} ({albumname}, {year})")
-    
-    data.append(",".join(
-            [trackname] + \
-            [artistnames[0]] + \
-            [",".join(["; ".join((artistnames[1:]) if len(artistnames) > 1 else "")])] + \
-            [albumname] + \
-            [year]
+    for item in itemcontainer:
+        # item_keys = ['added_at', 'added_by', 'is_local', 'primary_color', 'track', 'item', 'video_thumbnail']
+        # track_keys = ['preview_url', 'available_markets', 'explicit', 'type', 'episode', 'track', 'album', 'artists',
+        #               'disc_number', 'track_number', 'duration_ms', 'external_ids', 'external_urls', 'href', 'id',
+        #               'name', 'popularity', 'uri', 'is_local']
+        # artist_keys = ['external_urls', 'href', 'id', 'name', 'type', 'uri']
+        # album_keys = ['available_markets', 'type', 'album_type', 'href', 'id', 'images', 'name', 'release_date',
+        #               'release_date_precision', 'uri', 'artists', 'external_urls', 'total_tracks']
+        trackname = item["track"]["name"]
+        artistnames = [artist["name"] for artist in item["track"]["artists"]]
+        albumname = item["track"]["album"]["name"]
+        release_date = item["track"]["album"]["release_date"]
+        if release_date is not None:
+            year = release_date.split("-")[0]
+        else:
+            year = ""
+        print(f"{artistnames[0]} - {trackname} ({albumname}, {year})")
+        
+        data.append(",".join(
+                [trackname] + \
+                [artistnames[0]] + \
+                [",".join(["; ".join((artistnames[1:]) if len(artistnames) > 1 else "")])] + \
+                [albumname] + \
+                [year]
+            )
         )
-    )
 
-keys = ["Track", "Artist", "Other artists", "Album", "Year"] 
+    keys = ["Track", "Artist", "Other artists", "Album", "Year"] 
 
-if not os.path.exists("./csv"):
-    os.makedirs("./csv")
+    if not os.path.exists("./csv"):
+        os.makedirs("./csv")
 
 
-with open(f"csv/{playlist_name}.csv", "w", newline="", encoding="utf-8") as csvfile:
-    f = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    f.writerow(",".join(keys))
-    for row in data:
-        f.writerow(row)
+    with open(f"csv/{playlist_name}.csv", "w", newline="", encoding="utf-8") as csvfile:
+        f = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        f.writerow(",".join(keys))
+        for row in data:
+            f.writerow(row)
